4a7404249ed6e372810cf0a712051972282d47ce
EvdokimovViktor <evdokimov.viktor.91@mail.ru>
2021-02-09 20:07:53 +0500

    Task 2 debug

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,26 +1,21 @@
 import java.util.LinkedList;
 
 public class PhoneBook {
-    public static void main(String[] args) {
         LinkedList<PhoneNumber> phoneNumbers = new LinkedList<>();
-        addPhoneNumbers(phoneNumbers);
-        printNumbers(phoneNumbers, "Job");
-        printNumbers(phoneNumbers, "Anna");
 
-    }
-    public static void addPhoneNumbers (LinkedList<PhoneNumber> phoneNumbers){
-        phoneNumbers.add(new PhoneNumber(1516, "Job"));
-        phoneNumbers.add(new PhoneNumber(6321, "Anna"));
-        phoneNumbers.add(new PhoneNumber(6461, "Juli"));
-        phoneNumbers.add(new PhoneNumber(5371, "Rob"));
-        phoneNumbers.add(new PhoneNumber(9631, "Juli"));
-        phoneNumbers.add(new PhoneNumber(1542, "Tom"));
-        phoneNumbers.add(new PhoneNumber(5253, "Job"));
-    }
-    
-    public static void printNumbers (LinkedList<PhoneNumber> phoneNumbers, String name){
-        for(PhoneNumber number : phoneNumbers){
-            if(number.isThisName(name)) number.printInfo();
+        public void add (int number, String name){
+            phoneNumbers.add(new PhoneNumber(number,name));
         }
-    }
+        public void get (String name){
+            boolean isBookHaveNotThisName = true;
+            for (PhoneNumber phoneNumber: phoneNumbers){
+                if(name.equals(phoneNumber.getName())) {
+                    System.out.println(phoneNumber.toString());
+                    isBookHaveNotThisName = false;
+                }
+            }
+            if (isBookHaveNotThisName) System.out.printf("This book have not %s`s phone.", name);
+        }
+
+
 }
--- src/PhoneNumber.java
+++ src/PhoneNumber.java
@@ -7,11 +7,16 @@ public class PhoneNumber {
         this.phoneNumber = phoneNumber;
         this.name = name;
     }
-    public boolean isThisName (String searchName){
-        if (searchName.equals(name)) return true;
-        return false;
+
+    public String getName() {
+        return name;
     }
-    public void printInfo(){
-        System.out.printf("Phone number: %d Name: %s%n", phoneNumber, name);
+
+    @Override
+    public String toString() {
+        return "PhoneNumber{" +
+                "phoneNumber=" + phoneNumber +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
